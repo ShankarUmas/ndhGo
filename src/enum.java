5559690afdb31d2f2c688e9b3770861b2e672207
ShankarUmas <127491156+ShankarUmas@users.noreply.github.com>
2023-04-21 16:14:05 +0530

    Create enum.java

---
+++ src/enum.java
@@ -0,0 +1,15 @@
+enum Level{
+		Low,
+		Medium,
+		High,rnamg
+		}
+public class Demo {
+	public static void main(String[] args) {
+	Level mylevel = Level.High;
+System.out.println(mylevel);
+		for(Level muv:Level.values()) {
+			System.out.println(muv);
+		}
+	}
+
+}
